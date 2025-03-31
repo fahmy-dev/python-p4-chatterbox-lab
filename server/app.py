388eb4ff3b960f9c527de73a13944c40a917cf8b
fahmy-dev <faahmy@proton.me>
2025-03-31 22:51:08 +0300

    commit

--- server/app.py
+++ server/app.py
@@ -14,13 +14,59 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = [message.to_dict() for message in Message.query.all()]
 
-@app.route('/messages/<int:id>')
+        response = make_response(messages, 200)
+
+        return response
+    
+    elif request.method == 'POST':
+        data = request.get_json()
+
+        new_message = Message(body=data['body'], username=data['username'])
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        return make_response(new_message.to_dict(), 201)
+
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    if request.method == 'GET':
+        message = Message.query.filter(Message.id == id).first()
+
+        message_dict = message.to_dict()
+
+        response = make_response(message_dict, 200)
+
+        return response
+    
+    elif request.method == 'PATCH':
+        message = Message.query.filter(Message.id == id).first()
+
+        if message:
+            data = request.get_json()
+            message.body = data['body']
+
+            db.session.commit()
+
+            return make_response(message.to_dict(), 200)
+        
+        return make_response({'error': 'Message not found'}, 404)
+
+    elif request.method == 'DELETE':
+        message = Message.query.filter(Message.id == id).first()
+
+        if message:
+            db.session.delete(message)
+            db.session.commit()
+        
+            return make_response('', 204)
+
+        return make_response({'error': 'Message not found'}, 404)
 
 if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
+from datetime import datetime, timezone
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, func
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData(naming_convention={
@@ -12,3 +13,10 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+    username = db.Column(db.String)
+    created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc).astimezone())
+    updated_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc).astimezone(), onupdate=lambda: datetime.now(timezone.utc).astimezone())
+
+    def __repr__(self):
+        return f'<Message {self.id}: {self.body} by {self.username}: {self.created_at}: {self.updated_at}>'
